ba4da1d723ccf6e6dd9d966a5b78d0b8eea134d0
timsofteng <timsofteng@gmail.com>
2023-06-29 13:39:13 +0200

    fixed api

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 
-const fetchTodo = () =>
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const fetchTodo = () => fetch("https://jsonplaceholder.typicode.com/todos/1");
 
-export const useFetchTodo = ({ ...options }) => {
+export const useFetchTodo = ({ ...options } = {}) => {
   return useQuery({
     queryKey: ["fetch-todo"],
     queryFn: fetchTodo,
